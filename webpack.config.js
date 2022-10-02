71d687f57f81017ed561d9ac83c3e1357561242c
Usman Husnan <oets.man@gmail.com>
2022-10-02 21:25:28 +0700

    done with favicon

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
 	entry: "./src/index.js",
@@ -29,5 +30,10 @@ module.exports = {
 			filename: "index.html",
 		}),
 		new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /id/),
+		new FaviconsWebpackPlugin({
+			logo: "./src/logo.png",
+			inject: true,
+			prefix: "assets/favicon/",
+		}),
 	],
 };
